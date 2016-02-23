974d9457816a6607d6cf5d48f61eb289ed9057a3
Grey-Peters <seattlenewsguy@gmail.com>
2016-02-22 19:05:01 -0800

    Change to support removal of timestamps
    Timestamps not compatible with documentdb. Swapping to epoch style.

--- Prototype/metadata_and_JSON/makeMetadata.py
+++ Prototype/metadata_and_JSON/makeMetadata.py
@@ -1,29 +1,42 @@
 Python 3.3.2 (v3.3.2:d047928ae3f6, May 16 2013, 00:06:53) [MSC v.1600 64 bit (AMD64)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> 
-from forms import VoteForm
-import config
+>>>
 import pydocumentdb.document_client as document_client
+import verify_oauth
+import datetiem
+from datetime import timedelta
 	
 
 #Need a function specifically oriented toward making the call.
 #This is *not* async, which will need to change.
 #Ideally, after this is complete, we'd send the confirmation to the UI that the upload completed.
-def makeMetadata(date, url)
+def makeMetadata(user, originalFilename, tags, time, url, token, secret,)
 
-	client = document_client.DocumentClient(config.DOCUMENTDB_HOST, {'masterKey': config.DOCUMENTDB_KEY});
+	if verify_oauth(token, secret).status_code != 200:
+		return returnList["Could not verify oAuth credentials"];
+
+	epoc = datetime.datetime(2016, 2, 23, 3, 0, 00, 000000));
+	val = (time - epoc).total_seconds()*1000000;
+		
+	client = document_client.DocumentClient(DOCUMENTDB_HOST, {'masterKey': DOCUMENTDB_KEY});
 	
-	db = next((data for data in client.ReadDatabases() if data['id'] == config.DOCUMENTDB_DATABASE));
+	#Not sure we need this. Client may be it.
+	#db = next((data for data in client.ReadDatabases() if data['id'] == config.DOCUMENTDB_DATABASE));
 	
 	coll = next((coll for coll in client.ReadCollections(db['_self']) if coll['id'] == config.DOCUMENTDB_COLLECTION));
 
+	#create document. Tags is an array, as passed.
 	document = client.CreateDocument(coll['_self'],
-			{   "photo_url": url,
-				"photo_id": date,
-				"user_id": user,
+			{   "user_id": user,
 				"file_name": originalFilename,
-				"tag": tags,
+				"photo_url": url,
+				"photo_id": val,
+				"tags": tags,
 			});
-			
-	#this is not the intended end result.
-	return;
+	
+	returnlist = [];	
+	
+	if uploaded:
+		return returnList["Success", "File has been uploaded and added successfully."]
+	else:
+		return returnList["Failed to upload metadata to Azure DocumentDB"]

22f9a658e2215c75557e27c77f2e068f926763a6
Grey-Peters <seattlenewsguy@gmail.com>
2016-03-02 18:13:37 -0800

    Completed debug of getRecords code, and daily report

--- Prototype/api/FlaskApp/FlaskApp/getRecords.py
+++ Prototype/api/FlaskApp/FlaskApp/getRecords.py
@@ -1,23 +1,35 @@
+import pydocumentdb.document_client as document_client
+from static.app_keys import db_client, db_client_key, db_name, db_collection
+
+#helper method 
+def extract_val(json):
+	return int(json['photo_id'])
+
 def getRecords(user, lastID, direction, tags)
+	try:
+		dir = ">";
+		order = "ASC";
 
-	dir = ">";
-	order = "ASC";
+		if direction == True:
+			dir = "<";
+			order = "DESC";
+		
+		tagString = "[\""+'", "'.join(tags)+"\"]";
+		
+		client = document_client.DocumentClient(db_client, {'masterKey': db_client_key});
 
-	if direction == True:
-		dir = "<";
-		order = "DESC";
-	
-	tagString = "[\""+'", "'.join(tags)+"\"]";
-	
-	queryString = 'SELECT TOP 20 '+collectionName+'.user_id, '+collectionName+'.photo_id, '+collectionName+'.file_name, '+collectionName+'.photo_url, '+collectionName+'.tags FROM '+collectionName+' where '+collectionName+'.user_id = "'+user+'" and '+collectionName+'.tags = '+tagString+' ORDER BY '+collectionName+'.photo_id '+order+';';
-	
-	itterResult =  QueryDocuments(collection_link, queryString);
-	
-	sorted(itterResult, key="photo_id");
-	
-	jsnOb = flask.jsonify(**itterResult);
-	
-	if(direction == True){
-		return sorted(jsnOb, key="photo_id")
-	}
-	return jsnOb;
+		db = next((data for data in client.ReadDatabases() if data['id'] == db_name));
+		coll = next((coll for coll in client.ReadCollections(db['_self']) if coll['id'] == db_collection));
+		
+		queryString = 'SELECT TOP 20 '+db_collection+'.user_id, '+db_collection+'.photo_id, '+db_collection+'.file_name, '+db_collection+'.photo_url, '+db_collection+'.tags FROM '+db_collection+' where '+db_collection+'.user_id = "'+user+'" and '+db_collection+'.tags = '+tagString+' and '+db_collection+'.photo_id'+dir+lastID+' ORDER BY '+db_collection+'.photo_id '+order+';';
+		
+		itterResult =  QueryDocuments(coll, queryString);
+		
+		jsnOb = flask.jsonify(**itterResult);
+		
+		if(direction == True){
+			return sorted(jsnOb, key=extract_val(jsnOb), reverse=True);
+		}
+		return jsnOb;
+	except Exception:
+        return "error";
